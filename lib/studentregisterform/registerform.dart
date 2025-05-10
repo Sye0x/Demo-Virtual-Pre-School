55036ad33d08849fb0c8bc5a4c29e0e1e3992d3b
Sye0x <s.r.mahamid@gmail.com>
2025-05-11 04:07:53 +0500

    Adding student Admission form

--- lib/studentregisterform/registerform.dart
+++ lib/studentregisterform/registerform.dart
@@ -1,29 +1,45 @@
+import 'package:demo_vps/customwidgets/primarybuttonwidget.dart';
+import 'package:demo_vps/studentregisterform/registerformwidget.dart';
 import 'package:flutter/material.dart';
 
-class RegisterForm extends StatelessWidget {
+class RegisterForm extends StatefulWidget {
   const RegisterForm({super.key});
 
-  void register(BuildContext context) {
+  @override
+  RegisterFormState createState() => RegisterFormState();
+}
+
+class RegisterFormState extends State<RegisterForm> {
+  String buttonstr = "Next";
+
+  void forward(BuildContext context) {
     // Implement your registration logic here
+    setState(() {
+      buttonstr = "Submit"; // You can update this based on some logic
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      backgroundColor: Color(0xFF8C5FF5),
+      backgroundColor: const Color(0xFF8C5FF5),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              "Hello, Student!",
+            const Text(
+              "Application Form",
               style: TextStyle(
                 color: Color(0xFFFFFFFF),
-                fontSize: 40,
+                fontSize: 30,
                 fontWeight: FontWeight.bold,
               ),
             ),
+            const SizedBox(height: 30),
+            if (buttonstr == "Next") const RegisterFormWidget(),
+            if (buttonstr == "Submit") const SizedBox(height: 30),
+            Primarybuttonwidget(run: () => forward(context), input: buttonstr),
           ],
         ),
       ),
