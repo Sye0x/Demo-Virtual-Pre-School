8848e8970179dd4e3110836ff2b2705de74c90a3
Sye0x <s.r.mahamid@gmail.com>
2025-05-16 01:58:55 +0500

    Admin Pannel Header making

--- lib/admin/admindashboardscreen/admindashboardscreen.dart
+++ lib/admin/admindashboardscreen/admindashboardscreen.dart
@@ -11,9 +11,33 @@ class AdminDashboardScreenState extends State<AdminDashboardScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text(
-        "Admin Dashboard",
-        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: 40,
+              decoration: BoxDecoration(
+                color: const Color(0xFF8C5FF5),
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(15),
+                  bottomRight: Radius.circular(15),
+                ),
+              ),
+              child: const Center(
+                child: Text(
+                  "Admin Dashboard",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+          ],
+        ),
       ),
     );
   }
