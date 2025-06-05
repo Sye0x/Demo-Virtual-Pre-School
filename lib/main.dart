308a919a3f95daec784779b72d41cbf9e46d7b33
Sye0x <s.r.mahamid@gmail.com>
2025-06-05 19:45:49 +0500

    Firebase Connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:demo_vps/firebase_options.dart';
 import 'package:demo_vps/responsive.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:demo_vps/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Initialize Firebase with the default options
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
